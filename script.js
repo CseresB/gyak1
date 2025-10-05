cf3fd4f219466269c0405708f62a5eb847d313ab
CseresB <109346839+CseresB@users.noreply.github.com>
2025-10-05 22:37:34 +0200

    form control

--- script.js
+++ script.js
@@ -14,7 +14,19 @@ let p2 = document.getElementById("p2");
 p2.textContent="Changed content";
 }
 
+function processFormData{
+  //Access the form
+  let form = document.getElementById("myForm");
+  //Access the elements by name and get values
+  let city = form.elements.city;
+  let country = form.elements.country;
 
+  insertData(city,country);
+
+  //clear the input fields
+  document.getElementById("city").values = '';
+  document.getElementById("country").values='';
+}
 /*
 let userName = prompt("Please enter your username");
 document.getElementById('userName').textContent = userName;
